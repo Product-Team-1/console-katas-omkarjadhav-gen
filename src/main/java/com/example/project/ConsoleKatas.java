b7646049d1e10393bd5c01fb7c3d4025bcee626b
omkarjadhav-gen <167734630+omkarjadhav-gen@users.noreply.github.com>
2025-10-06 13:27:15 +0530

    Revised even or odd function

--- src/main/java/com/example/project/ConsoleKatas.java
+++ src/main/java/com/example/project/ConsoleKatas.java
@@ -68,7 +68,7 @@ public class ConsoleKatas {
         }
 
     public static String evenOrOdd(int n) {
-        if (n % 3 == 0) {
+        if (n % 2 == 0) {
             return "Even";
         } else {
             return "Odd";
