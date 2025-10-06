345bcb59d6acbb707dd28f11a527280dcb8f15db
omkarjadhav-gen <167734630+omkarjadhav-gen@users.noreply.github.com>
2025-10-06 09:30:51 +0530

    Sample Even and Odd Program 2

--- src/main/java/com/example/project/ConsoleKatas.java
+++ src/main/java/com/example/project/ConsoleKatas.java
@@ -2,12 +2,10 @@ package com.example.project;
 
 public class ConsoleKatas {
 
-    // Function to check even or odd
     public static String evenOrOdd(int n) {
-        if (n % 2 == 0) {
+if (n % 2 == 0) {
             return "Even";
         } else {
             return "Odd";
         }
-    }
-}
+        }
