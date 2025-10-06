c0c96a1eeabf253952f90cfeb3450377b1d2900d
omkarjadhav-gen <167734630+omkarjadhav-gen@users.noreply.github.com>
2025-10-06 09:42:44 +0530

    Incorrect function

--- src/main/java/com/example/project/ConsoleKatas.java
+++ src/main/java/com/example/project/ConsoleKatas.java
@@ -68,10 +68,6 @@ public class ConsoleKatas {
         }
 
     public static String evenOrOdd(int n) {
-        if (n % 2 == 0) {
-            return "Even";
-        } else {
-            return "Odd";
-        }
+        
     }
 }
